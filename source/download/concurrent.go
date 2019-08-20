4aadbb397376d2e45d1de198573324bc89d91414
Saurabh Kumar <saurabhk442@gmail.com>
2019-08-20 12:40:49 +0530

    Concurrent Download

--- source/download/concurrent.go
+++ source/download/concurrent.go
@@ -1,8 +1,10 @@
 package download
 
 import (
+	"encoding/json"
+	"github.com/skcse/DownloadManager/source/status"
 	"net/http"
-	"sync"
+	"time"
 )
 
 type concurrent struct {
@@ -10,9 +12,53 @@ type concurrent struct {
 }
 
 func (c concurrent) startDownload(writer http.ResponseWriter,request *http.Request,urls []string)  {
-	var wg sync.WaitGroup
-	wg.Add(1)
-	//req:=make(chan string)
-	//
-	//go gatherResults(posMap, resChan, reqChan,len(words), &wg)
+	startTime:=time.Now()
+	mapFiles:=make(map[string]string)
+	finalMapFiles:=make(map[string]string)
+	for _,url:=range urls{
+		name := generateId()
+		mapFiles[url]=name
+	}
+	const limitWorker =2
+	reqChan:=make(chan string)
+
+	for i:=0;i<limitWorker;i++{
+		go worker(reqChan,mapFiles,finalMapFiles,len(urls))
+	}
+
+	id:= generateId()
+	idData:= downloadId{Id: id}
+	writer.Header().Set("Content-Type", "application/json")
+	js,_:=json.Marshal(idData)
+
+	status.Mp[id]= status.Status{id,startTime,time.Now(),"QUEUED","CONCURRENT",mapFiles}
+
+	go func() {
+		for _, url := range urls {
+			reqChan <- url
+		}
+		status.Mp[id]= status.Status{id,startTime,time.Now(),"SUCCESSFUL","CONCURRENT",mapFiles}
+		return
+	}()
+
+	writer.Write(js)
 }
+
+func worker(reqChan chan string,mapFiles map[string]string,finalMapFiles map[string]string,urlsCount int)  {
+	for {
+		select {
+			case url,ok := <-reqChan:
+				name:=mapFiles[url]
+				_ = downloadFile(url, name)
+				finalMapFiles[url] = name
+				if !ok {
+					return
+				}
+			}
+			if urlsCount == len(finalMapFiles){
+				close(reqChan)
+				return
+			}
+		}
+}
+
